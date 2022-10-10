2f1bd6ae0952c64ae4805dd8734348bead66145e
panosjapan7 <panosjapan@gmail.com>
2022-10-10 13:29:36 +0200

    Connects to, Saves to and gets data from  MongoDB db

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, json, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import ChatItem from "@mernt-chat-app/shared";
+import { setupMongoDb, loadAllChatItems, saveChatItem } from "./db";
 
 dotenv.config();
 
@@ -11,21 +12,28 @@ app.use(cors());
 app.use(json()); // middleware Express function that parses incoming JSON requests and puts the parsed data in req.body.
 
 const PORT: number = parseInt(process.env.SERVER_PORT || "3001");
+const mongoUrl:string = process.env.MONGODB_URL || "mongodb://localhost:27017";
 
-const CHAT_ITEMS:ChatItem[] = [{_id: "123", text: "My first message! (via backend)", timeStamp: new Date(), author: "Panos"}];
+// const CHAT_ITEMS:ChatItem[] = [{_id: "123", text: "My first message! (via backend)", timeStamp: new Date(), author: "Panos"}];
 
-app.get("/chats", (req: Request, res: Response<ChatItem[]>) => {
-    res.send(CHAT_ITEMS);
+app.get("/chats", async (req: Request, res: Response<ChatItem[]>) => {
+    const chatItems = await loadAllChatItems();
+    console.log("All chat items: ", chatItems);
+    res.send(chatItems);
 });
 
-app.post("/chats", (req: Request<ChatItem>, res: Response<ChatItem[]>) => {
+app.post("/chats", async (req: Request<ChatItem>, res: Response<ChatItem[]>) => {
     const chatItem = req.body;
-    console.log("Got a new message: ", chatItem);
-    CHAT_ITEMS.push(chatItem);
-    res.send(CHAT_ITEMS);
+    const saved = await saveChatItem(chatItem);
+    console.log("Saves Chat Item: ", chatItem);
+    // CHAT_ITEMS.push(chatItem);
+    const chatItems = await loadAllChatItems();
+    console.log("All Chat Items: ", chatItems);
+    res.send(chatItems);
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, async function() {
+    await setupMongoDb(mongoUrl);
     console.log(`App is listening on ${PORT}`);
 });
 
