dc87cce649fd13cd68edffc7b28fd60b90284261
panosjapan7 <panosjapan@gmail.com>
2022-10-10 11:37:19 +0200

    Creates functions to set up MongoDb, load all ChatItems, save a ChatItem to a new model

--- packages/backend/src/db.ts
+++ packages/backend/src/db.ts
@@ -7,3 +7,17 @@ const ChatItemSchema = new Schema({
 });
 
 const ChatItemModel = model<ChatItem>("ChatItem", ChatItemSchema);
+
+export const setupMongoDb = async (url: string) => {
+    await connect(url);
+};
+
+export const loadAllChatItems = async (): Promise<ChatItem[]> => {
+    return ChatItemModel.find({}).exec()
+};
+
+export const saveChatItem = async (chatItem: ChatItem): Promise<void> => {
+    const newModel = new ChatItemModel(chatItem);
+    newModel.save();
+};
+
