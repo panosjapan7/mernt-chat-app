b68408f6b1ea5ebe45b8358e14852a63335a6e24
panosjapan7 <panosjapan@gmail.com>
2022-10-10 09:11:46 +0200

    Imports ChatItem interface, changes get() endpoint to /chats, sends a ChatItem as response

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, json, Request, Response } from "express";
 import cors from "cors";
+import ChatItem from "@mernt-chat-app/shared";
 
 const app: Application = express();
 
@@ -8,8 +9,8 @@ app.use(json()); // middleware Express function that parses incoming JSON reques
 
 const PORT: number = parseInt(process.env.SERVER_PORT || "3001");
 
-app.get("/hello", (req: Request, res: Response) => {
-    res.send("Hello, world");
+app.get("/chats", (req: Request, res: Response<ChatItem>) => {
+    res.send({_id: "123", text: "My first message!", timeStamp: new Date(), author: "Panos"});
 });
 
 app.listen(PORT, function() {
