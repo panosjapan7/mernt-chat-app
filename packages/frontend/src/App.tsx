a2d979cf202a1f818516fa656e8fcc95a804d667
panosjapan7 <panosjapan@gmail.com>
2022-10-10 14:03:36 +0200

    Shows login component if author is undefined, Chat items if it is

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import './App.css';
 import ChatItem from "@mernt-chat-app/shared";
+import { LoginInput } from "./LoginInput";
 
 axios.defaults.baseURL = process.env.REACT_APP_CHAT_API || "http://localhost:3001";
 
@@ -15,22 +16,30 @@ function App() {
   const [chatItems, setChatItems] = useState<ChatItem[]>([]);
   const [error, setError] = useState<string | undefined>();
   const [chatItemText, setChatItemText] = useState<string>("");
+  
+  const [author, setAuthor] = useState<string>("");
 
   const createChatItem = async (chatItemText: string): Promise<void> => {
     const chatItem: ChatItem = {
       text: chatItemText, 
       timeStamp: new Date(),
-      author: "Panos",
-      // _id: "123"
+      author: author,
     };
     try {
       const response = await axios.post<ChatItem[]>("/chats", chatItem);
       setChatItems(response.data);
+      setChatItemText("");
     } 
     catch (error) {
       setChatItems([]);
       setError("Something went wrong when fetching array with messages...");
     }
+  };
+
+  const performLogin = async (author: string): Promise<void> => {
+    setAuthor(author);
+    console.log(author);
+
   };
 
   useEffect(() => {
@@ -48,19 +57,29 @@ function App() {
 
   return (
     <div>
-      <div className='container--chatItem'>
-        {chatItems && chatItems.length !== 0 ? chatItems.map( chatItem => (
-                        <div key={chatItem._id}>
-                          <p>{chatItem.text}</p>
-                        </div>
-        )
-          ) : error ? error : "No messages sent..."}
-      </div>
+      {!author && <LoginInput onLogin={performLogin} />}
+      
+      {author && (  
+        <div className='container--chatItems'>
+          <div className='container--author'>
+            <p>Author: {author}</p>
+          </div>
 
-      <section>
-        <input type="text" value={chatItemText} onChange={(e) => setChatItemText(e.target.value)}/>
-        <button onClick={(e) => createChatItem(chatItemText)}>Send</button>
-      </section>
+          <div className='container--chatItem'>
+            {chatItems && chatItems.length !== 0 ? chatItems.map( chatItem => (
+                            <div key={chatItem._id}>
+                              <p>{chatItem.text}</p>
+                            </div>
+            )
+              ) : error ? error : "No messages sent..."}
+          </div>
+
+          <section>
+            <input type="text" value={chatItemText} onChange={(e) => setChatItemText(e.target.value)}/>
+            <button onClick={(e) => createChatItem(chatItemText)}>Send</button>
+          </section>
+        </div>
+      )}
     </div>
   );
 }
