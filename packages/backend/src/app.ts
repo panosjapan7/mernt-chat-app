1f604fafa0434fe8d006d9e888ae854a2c110044
panosjapan7 <panosjapan@gmail.com>
2022-11-03 13:10:19 +0100

    Deletes console.logs

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -16,23 +16,18 @@ const mongoUrl:string = process.env.MONGODB_URL || "mongodb://localhost:27017";
 
 app.get("/chats", async (req: Request, res: Response<ChatItem[]>) => {
     const chatItems = await loadAllChatItems();
-    console.log("All chat items: ", chatItems);
     res.send(chatItems);
 });
 
 app.post("/chats", async (req: Request<ChatItem>, res: Response<ChatItem[]>) => {
     const chatItem = req.body;
     const saved = await saveChatItem(chatItem);
-    console.log("Saves Chat Item: ", chatItem);
-    // CHAT_ITEMS.push(chatItem);
     const chatItems = await loadAllChatItems();
-    console.log("All Chat Items: ", chatItems);
     res.send(chatItems);
 });
 
 app.listen(PORT, async function() {
     await setupMongoDb(mongoUrl);
-    console.log(`App is listening on ${PORT}`);
 });
 
 
